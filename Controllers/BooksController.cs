de68709681cf8d9d8117ee941bcebb79b42f0c0a
Hugo Molaudzi <57953645+In-Cyberspace@users.noreply.github.com>
2020-06-09 18:09:17 +0200

    Created the books controller and implemented initial dependency injections for future use within the class.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// Interacts with the books table.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+
+        public BooksController(IBookRepository bookRepository,
+            ILoggerService logger,
+            IMapper mapper)
+        {
+            _bookRepository = bookRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+    }
+}
