ba3b9a58118900223e713bc7816907a3c69773f4
Hugo Molaudzi <57953645+In-Cyberspace@users.noreply.github.com>
2020-06-10 11:24:43 +0200

    Implemented the GET endpoints in the books controller.

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using BookStore_API.Contracts;
+using BookStore_API.Data;
+using BookStore_API.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -27,6 +28,81 @@ namespace BookStore_API.Controllers
             _bookRepository = bookRepository;
             _logger = logger;
             _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get all books.
+        /// </summary>
+        /// <returns>A list of books.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetBooks()
+        {
+            string location = GetControllerActionNames();
+
+            try
+            {
+                _logger.LogInfo($"{location}: Attempted call.");
+                IList<Book> books = await _bookRepository.FindAll();
+                IList<BookDTO> response = _mapper.Map<IList<BookDTO>>(books);
+                _logger.LogInfo($"{location}: Successful.");
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return InternalError($"{location}: {e.Message} - {e.InnerException}");
+            }
+        }
+
+        /// <summary>
+        /// Gets a book by Id.
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns>A book record.</returns>
+        [HttpGet("{Id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetBook(int Id)
+        {
+            string location = GetControllerActionNames();
+
+            try
+            {
+                _logger.LogInfo($"{location}: Attempted call for Id: {Id}.");
+                Book book = await _bookRepository.FindById(Id);
+
+                if (book == null)
+                {
+                    _logger.LogWarn($"{location}: Failed to retrieve record with Id: {Id}.");
+                    return NotFound();
+                }
+
+                BookDTO response = _mapper.Map<BookDTO>(book);
+                _logger.LogInfo($"{location}: Successful got record with Id: {Id}.");
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return InternalError($"{location}: {e.Message} - {e.InnerException}");
+            }
+        }
+
+        private string GetControllerActionNames()
+        {
+            string controller = ControllerContext.ActionDescriptor.ControllerName;
+            string action = ControllerContext.ActionDescriptor.ActionName;
+            
+            return $"{controller} - {action}";
+        }
+
+        private ObjectResult InternalError(string message)
+        {
+            _logger.LogError(message);
+
+            return StatusCode(500, "Something went wrong. Please contact the administrator.");
         }
     }
 }
