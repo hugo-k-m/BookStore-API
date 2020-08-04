0c566b0a5eb9e9fc31feff24320fea769f68b597
Hugo Molaudzi <57953645+hugo-k-m@users.noreply.github.com>
2020-08-04 13:05:50 +0200

    Setup author repository and service.

---
+++ BookStore-UI/Service/AuthorRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BookStore_UI.Contracts;
+using BookStore_UI.Models;
+
+namespace BookStore_UI.Service
+{
+    public class AuthorRepository : BaseRepository<Author>, IAuthorRepository
+    {
+        private IHttpClientFactory _client;
+
+        public AuthorRepository(IHttpClientFactory client) : base(client)
+        {
+            _client = client;
+        }
+    }
+}
